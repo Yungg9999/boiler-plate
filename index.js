8d991873a5078859212788a6fa7016d8d4f64ce4
Yungg9999 <109264144+Yungg9999@users.noreply.github.com>
2023-04-12 11:22:30 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3500
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require("./models/User1")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Seo:yun1329@boiler-plate.0bzeppm.mongodb.net/?retryWrites=true&w=majority')
+
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~~~')
+  res.send('Hello World!~~~~새해복많이')
 })
 
+app.post('/register', (req, res) => {
+  
+  //회원가입에 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body);
+        //user모델에 정보가 저장됨
+        //실패 시, 실패한 정보를 보내줌
+        user.save().then(()=>{
+            res.status(200).json({
+                success:true
+            })
+        }).catch((err)=>{
+            return res.json({success:false,err})
+        });
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
